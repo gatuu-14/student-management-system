123676dfbb5e6886160f797aabe64e57f5b8f4d5
gatuu-14 <gatwirilinda87@gmail.com>
2025-01-17 17:09:10 +0300

    made changes on my database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Student(models.Model):
+
+    user_name = models.CharField(max_length=100)
+    registration_number = models.CharField(max_length=20, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    fee_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/avatar.svg')
+    dom = models.CharField(max_length=100, null=True, blank=True)
+    room_no = models.CharField(max_length=100, null=True, blank=True )
+    parents_phone = models.CharField(max_length=100, null=True, blank=True)
+    parents_name = models.CharField(max_length=100, null=True, blank=True)
+    religion = models.CharField(max_length=100, null=True, blank=True)
+    club = models.CharField(max_length=100, null=True, blank=True)
+    
+
+    def  __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+def student_list(request):
+    # Fetch all students from the database
+    students = Student.objects.all()
+    
+    # Render the 'student_list.html' template and pass the students list
+    return render(request, 'student_list.html', {'students': students})
